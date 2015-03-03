0c6e7713fc1fb538ac7c6a97ab01c35f8503cfd6
jens <jens@lea.io>
2015-03-03 15:00:45 +0100

    Adding -windowed commandline flag for better debugging

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -2,8 +2,14 @@
 #include "ofApp.h"
 
 //========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+int main( int argc, char *argv[] ){
+  
+  // check if launched from Xcode
+  if (argc > 1 && strcmp(argv[1], "-windowed") == 0) {
+    ofSetupOpenGL(1366,768,OF_WINDOW);			// <-------- setup the GL context
+  } else {
+    ofSetupOpenGL(1366,768,OF_FULLSCREEN);			// <-------- setup the GL context
+  }
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
