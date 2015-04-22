67db48d0e1914d9ec0bc9d496593e3e24cfafa1c
jens <jens@lea.io>
2015-04-22 11:56:08 +0200

    Server writes new comment into db

--- router/etc/chilli/www/comment.lua
+++ router/etc/chilli/www/comment.lua
@@ -1,6 +1,8 @@
 -- textinstallation - comment receiver script
 -- jens alexander ewald, lea.io, 2015
 
+local db = "/root/textinstallation/db/zitate.csv"
+
 -- Import the JSON en-/decoder
 JSON = (loadfile "JSON.lua")()
 
@@ -17,6 +19,51 @@ local lines = function(str)
   helper((str:gsub("(.-)\r?\n", helper)))
   return t
 end
+-- from http://lua-users.org/wiki/CsvUtils
+-- Used to escape "'s by toCSV
+function escapeCSV (s)
+  if string.find(s, '[,"]') then
+    s = '"' .. string.gsub(s, '"', '""') .. '"'
+  end
+  return s
+end
+-- Convert from CSV string to table (converts a single line of a CSV file)
+function fromCSV (s)
+  s = s .. ','        -- ending comma
+  local t = {}        -- table to collect fields
+  local fieldstart = 1
+  repeat
+    -- next field is quoted? (start with `"'?)
+    if string.find(s, '^"', fieldstart) then
+      local a, c
+      local i  = fieldstart
+      repeat
+        -- find closing quote
+        a, i, c = string.find(s, '"("?)', i+1)
+      until c ~= '"'    -- quote not followed by quote?
+      if not i then error('unmatched "') end
+      local f = string.sub(s, fieldstart+1, i-1)
+      table.insert(t, (string.gsub(f, '""', '"')))
+      fieldstart = string.find(s, ',', i) + 1
+    else                -- unquoted; find next comma
+      local nexti = string.find(s, ',', fieldstart)
+      table.insert(t, string.sub(s, fieldstart, nexti-1))
+      fieldstart = nexti + 1
+    end
+  until fieldstart > string.len(s)
+  return t
+end
+-- Convert from table to CSV string
+function toCSV (tt)
+  local s = ""
+  -- ChM 23.02.2014: changed pairs to ipairs
+  -- assumption is that fromCSV and toCSV maintain data as ordered array
+  for _,p in ipairs(tt) do
+    s = s .. "," .. escapeCSV(p)
+  end
+  return string.sub(s, 2)      -- remove first comma
+end
+
 -- END Helper Functions
 
 local data = {}
@@ -41,6 +88,12 @@ end
 -- Decode the JSON from the found query
 local input = JSON:decode(query)
 
+if input == nil then
+  input = {}
+  input["name"] = ""
+  input["comment"] = ""
+end
+
 -- Fix anonymous visitor
 if input.name == nil or string.len(input.name) == 0 then
   input.name = "Ein Besucher"
@@ -49,6 +102,14 @@ end
 data["comment"] = input
 
 -- add the comment to the DB
+local f,err,status = io.open(db, "a")
+
+-- Zitat,Name,Position,Jahr,Anlass(which is 'comment' here)
+local entry = escapeCSV(input.comment)..","..escapeCSV(input.name)..",,"..os.date("%Y")..",comment\n"
+os.execute("logger -t \"kommentar.user.comment.csv\" '" .. entry .. "'\n")
+
+f:write(entry)
+f:close()
 
 -- notify clients to get new DB
 
